55f4bfc066fcf388fa624e7c1043c0902aadaab8
p43gupta <p43gupta@scsltxxx>
2019-02-07 17:37:39 -0500

    server functions

--- server/nfsServer.cc
+++ server/nfsServer.cc
@@ -1,59 +1,77 @@
+#include <sys/stat.h> 	// mkdir
+#include <unistd.h>		// rmdir
 
-#include <grpc/grpc.h>
-#include <grpcpp/server.h>
-#include <grpcpp/server_builder.h>
-#include <grpcpp/server_context.h>
-#include <grpcpp/security/server_credentials.h>
-
-#include "../proto/schema.grpc.pb.h"
-	
-using grpc::Server;
-using grpc::ServerBuilder;
-using grpc::ServerContext;
-using grpc::ServerReader;
-using grpc::ServerReaderWriter;
-using grpc::ServerWriter;
-using grpc::Status;
-
-using NfsProtocol::mkdir_response;
-using NfsProtocol::mkdir_request;
-using NfsProtocol::NfsServer;
+#include "nfsServer.h"
 
 
-
-class serverImplementation final : public NfsServer::Service {
-
-	public :
-
-		Status mkdir(ServerContext * context, const mkdir_request * request, mkdir_response * response) override {
-
-			return Status::OK;
-		}
-
-
-};
-
-
-
-
-void runServer() {
-	
-	std::string server_address("0.0.0.0:3110");
-	
-	serverImplementation service;
-
-	ServerBuilder builder;
-	builder.AddListeningPort(server_address, grpc::InsecureServerCredentials());
-	builder.RegisterService(&service);
-	std::unique_ptr<Server> server(builder.BuildAndStart());
-	std::cout << "Server listening on " << server_address << std::endl;
-	server->Wait();
+serverImplementation::serverImplementation(std::string path) {
+	Service();
+	this->base = path;
 }
 
 
-int main(int argc, char** argv) {
-	runServer();
-	return 0;	
+Status serverImplementation::server_mkdir(ServerContext * context, const mkdir_request * request, c_response * response) {
+
+	std::string newDir = this->base + request->dirfh() + request->name();
+	mode_t mode = request->attr().st_mode();
+	char * newDirChars;
+	newDirChars = (char *)malloc(newDir.length() + 1);
+	strcpy(newDirChars, newDir.c_str());
+
+	if(mkdir(newDirChars, mode) != 0) {
+		response->set_success(-1);
+		response->set_ern(errno);
+	}
+	else {
+		response->set_success(0);
+		response->set_ern(0);	
+	}
+	return 	Status::OK;	
+
 }
+
+Status serverImplementation::server_rmdir(ServerContext * context, const rmdir_request * request, c_response * response) {
+
+	std::string dirToRemove = this->base + request->dirfh();
+	char * dirToRemoveChars;
+	dirToRemoveChars = (char *)malloc(dirToRemove.length() + 1);
+	strcpy(dirToRemoveChars, dirToRemove.c_str());
+
+	if(rmdir(dirToRemoveChars	) != 0) {
+		response->set_success(-1);
+		response->set_ern(errno);
+	}
+	else {
+		response->set_success(0);
+		response->set_ern(0);	
+	}
+	return 	Status::OK;	
+
+}
+
+Status serverImplementation::server_rename(ServerContext * context, const rename_request * request, c_response * response) {
+
+	std::string toDir = this->base + request->todir();
+	char * toDirChars;
+	toDirChars = (char *)malloc(toDir.length() + 1);
+	strcpy(toDirChars, toDir.c_str());
+
+	std::string fromDir = this->base + request->fromdir();
+	char * fromDirChars;
+	fromDirChars = (char *)malloc(fromDir.length() + 1);
+	strcpy(fromDirChars, fromDir.c_str());
+
+	if(rename(fromDirChars, toDirChars) != 0) {
+		response->set_success(-1);
+		response->set_ern(errno);
+	}
+	else {
+		response->set_success(0);
+		response->set_ern(0);	
+	}
+	return 	Status::OK;	
+
+}
+
 
 
---
+++ server/nfsServer.h
@@ -0,0 +1,48 @@
+#ifndef NFSSERVER
+#define NFSSERVER
+
+#include <sys/stat.h>
+
+#include <grpc/grpc.h>
+#include <grpcpp/server.h>
+#include <grpcpp/server_builder.h>
+#include <grpcpp/server_context.h>
+#include <grpcpp/security/server_credentials.h>
+
+#include "../proto/schema.grpc.pb.h"
+
+using grpc::Server;
+using grpc::ServerBuilder;
+using grpc::ServerContext;
+using grpc::ServerReader;
+using grpc::ServerReaderWriter;
+using grpc::ServerWriter;
+using grpc::Status;
+
+using NfsProtocol::mkdir_request;
+using NfsProtocol::rmdir_request;
+using NfsProtocol::rename_request;
+using NfsProtocol::c_response;
+using NfsProtocol::NfsServer;
+
+
+class serverImplementation final : public NfsServer::Service {
+
+	private :
+
+		std::string base;
+
+	public :
+
+		serverImplementation(std::string path);
+		
+		Status server_mkdir(ServerContext * context, const mkdir_request * request, c_response * response) override;
+		Status server_rmdir(ServerContext * context, const rmdir_request * request, c_response * response) override;
+		
+		Status server_rename(ServerContext * context, const rename_request * request, c_response * response) override;
+
+
+
+};
+
+#endif 
