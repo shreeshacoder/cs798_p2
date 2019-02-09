349f8505b4ba5c08a5e9acf7ec838b11842ec00f
BlacknMellow <shreesha.addala2012@vit.ac.in>
2019-02-08 23:26:42 -0500

    Implement Readdir

--- client/nfsClient.cc
+++ client/nfsClient.cc
@@ -1,19 +1,19 @@
 #include "nfsClient.h"
 
-using NfsProtocol::mkdir_request;
 using NfsProtocol::c_response;
+using NfsProtocol::mkdir_request;
 using NfsProtocol::NfsServer;
 
 clientImplementation::clientImplementation(std::shared_ptr<Channel> channel)
-      : stub_(NfsServer::NewStub(channel)) {}
+	: stub_(NfsServer::NewStub(channel)) {}
 
-
-int clientImplementation::client_mkdir(std::string path, mode_t mode) {
+int clientImplementation::client_mkdir(std::string path, mode_t mode)
+{
 
 	// mkdir_request request;
 	// c_response response;
 	// ClientContext context;
-	
+
 	// request.set_path(path);
 	// request.set_mode(mode);
 
@@ -27,29 +27,64 @@ int clientImplementation::client_mkdir(std::string path, mode_t mode) {
 	// }
 	// else {
 	// 	std::cout << status.error_code() << ": " << status.error_message() << std::endl;
- //    	return -1;
+	//    	return -1;
 	// }
-
 }
 
-int clientImplementation::client_rmdir(std::string path) {
+int clientImplementation::client_rmdir(std::string path)
+{
 
 	return 0;
 }
 
-int clientImplementation::client_rename(std::string from, std::string to) {
+int clientImplementation::client_rename(std::string from, std::string to)
+{
 
 	return 0;
 }
 
-int clientImplementation::client_open(std::string path, struct fuse_file_info *fi) {
+int clientImplementation::client_open(std::string path, struct fuse_file_info *fi)
+{
 
 	return 0;
 }
 
-int clientImplementation::client_create(std::string path, mode_t mode, struct fuse_file_info *fi) {
+int clientImplementation::client_create(std::string path, mode_t mode, struct fuse_file_info *fi)
+{
 
 	return 0;
 }
 
+std::list<DirEntry> clientImplementation::read_directory(std::string path, int &responseCode)
+{
+	// Container request
+	ReadDirectoryRequestObject readDirectoryRequestObject;
+	readDirectoryRequestObject.set_path(path);
+	ClientContext context;
 
+	// Container response
+	ReadDirectoryResponseObject readDirectoryResponseObject;
+
+	// Call
+	Status status = stub_->read_directory(&context, readDirectoryRequestObject, &readDirectoryResponseObject);
+	std::list<DirEntry> entries;
+	if (status.ok())
+	{
+		responseCode = readDirectoryResponseObject.status();
+		for (int i = 0; i < readDirectoryResponseObject.objects_size(); i++)
+		{
+			DirEntry dirEntry;
+			toCstat(readDirectoryResponseObject.objects(i).st(), &dirEntry.st);
+			dirEntry.name = readDirectoryResponseObject.objects(i).name();
+			entries.push_back(dirEntry);
+		}
+		return entries;
+	}
+	else
+	{
+		if (LOG)
+			std::cout << status.error_code() << ": " << status.error_message()
+					  << std::endl;
+		return entries;
+	}
+}
--- client/wrapper.cc
+++ client/wrapper.cc
@@ -4,158 +4,194 @@
 
 static clientImplementation nfsClient(grpc::CreateChannel("0.0.0.0:3110", grpc::InsecureChannelCredentials()));
 
-std::string toStr(const char * path) {
+std::string toStr(const char *path)
+{
 	std::string a(path);
 	return a;
 }
 
-int wrapper_getattr(const char *path, struct stat *statbuf, struct fuse_file_info *fi) {
+int wrapper_getattr(const char *path, struct stat *statbuf, struct fuse_file_info *fi)
+{
 	printf("wrapper get attr\n");
 	return 0;
 }
 
-int wrapper_readlink(const char *path, char *link, size_t size) {
+int wrapper_readlink(const char *path, char *link, size_t size)
+{
 	printf("wrapper read link\n");
 	return 0;
 }
 
-int wrapper_mknod(const char *path, mode_t mode, dev_t dev) {
+int wrapper_mknod(const char *path, mode_t mode, dev_t dev)
+{
 	printf("wrapper mknod\n");
 	return 0;
 }
 
-int wrapper_mkdir(const char *path, mode_t mode) {
+int wrapper_mkdir(const char *path, mode_t mode)
+{
 	printf("wrapper mkdir\n");
 	return nfsClient.client_mkdir(toStr(path), mode);
 }
 
-int wrapper_unlink(const char *path) {
+int wrapper_unlink(const char *path)
+{
 	printf("wrapper unlink\n");
 	return 0;
 }
 
-int wrapper_rmdir(const char *path) {
+int wrapper_rmdir(const char *path)
+{
 	printf("wrapper rmdir\n");
 	return nfsClient.client_rmdir(toStr(path));
 }
 
-int wrapper_symlink(const char *path, const char *link) {
+int wrapper_symlink(const char *path, const char *link)
+{
 	printf("wrapper symlink\n");
 	return 0;
 }
 
-int wrapper_rename(const char *path, const char *newpath, unsigned int flag) {
+int wrapper_rename(const char *path, const char *newpath, unsigned int flag)
+{
 	printf("wrapper rename\n");
 	return nfsClient.client_rename(toStr(path), toStr(newpath));
 }
 
-int wrapper_link(const char *path, const char *newpath) {
+int wrapper_link(const char *path, const char *newpath)
+{
 	printf("wrapper link\n");
 	return 0;
 }
 
-int wrapper_chmod(const char *path, mode_t mode) {
+int wrapper_chmod(const char *path, mode_t mode)
+{
 	printf("wrapper chmod\n");
 	return 0;
 }
 
-int wrapper_create(const char * path, mode_t mode, struct fuse_file_info * fileInfo) {
+int wrapper_create(const char *path, mode_t mode, struct fuse_file_info *fileInfo)
+{
 	printf("wrapper create\n");
 	return nfsClient.client_create(toStr(path), mode, fileInfo);
 }
 
-int wrapper_chown(const char *path, uid_t uid, gid_t gid) {
+int wrapper_chown(const char *path, uid_t uid, gid_t gid)
+{
 	printf("wrapper chown\n");
 	return 0;
 }
 
-int wrapper_truncate(const char *path, off_t newSize, struct fuse_file_info *fi) {
+int wrapper_truncate(const char *path, off_t newSize, struct fuse_file_info *fi)
+{
 	printf("wrapper truncate\n");
 	return 0;
 }
 
-int wrapper_utime(const char *path, struct utimbuf *ubuf) {
+int wrapper_utime(const char *path, struct utimbuf *ubuf)
+{
 	printf("wrapper utime\n");
 	return 0;
 }
 
-int wrapper_open(const char *path, struct fuse_file_info *fileInfo) {
+int wrapper_open(const char *path, struct fuse_file_info *fileInfo)
+{
 	printf("wrapper open\n");
 	return nfsClient.client_open(toStr(path), fileInfo);
 }
 
-int wrapper_read(const char *path, char *buf, size_t size, off_t offset, struct fuse_file_info *fileInfo) {
+int wrapper_read(const char *path, char *buf, size_t size, off_t offset, struct fuse_file_info *fileInfo)
+{
 	printf("wrapper read\n");
 	return 0;
 }
 
-int wrapper_write(const char *path, const char *buf, size_t size, off_t offset, struct fuse_file_info *fileInfo) {
+int wrapper_write(const char *path, const char *buf, size_t size, off_t offset, struct fuse_file_info *fileInfo)
+{
 	printf("wrapper write\n");
 	return 0;
 }
 
-int wrapper_statfs(const char *path, struct statvfs *statInfo) {
+int wrapper_statfs(const char *path, struct statvfs *statInfo)
+{
 	printf("wrapper statfs\n");
 	return 0;
 }
 
-int wrapper_flush(const char *path, struct fuse_file_info *fileInfo) {
+int wrapper_flush(const char *path, struct fuse_file_info *fileInfo)
+{
 	printf("wrapper flush\n");
 	return 0;
 }
 
-int wrapper_release(const char *path, struct fuse_file_info *fileInfo) {
+int wrapper_release(const char *path, struct fuse_file_info *fileInfo)
+{
 	printf("wrapper release\n");
 	return 0;
 }
 
-int wrapper_fsync(const char *path, int datasync, struct fuse_file_info *fi) {
+int wrapper_fsync(const char *path, int datasync, struct fuse_file_info *fi)
+{
 	printf("wrapper fsync\n");
 	return 0;
 }
 
-int wrapper_setxattr(const char *path, const char *name, const char *value, size_t size, int flags) {
+int wrapper_setxattr(const char *path, const char *name, const char *value, size_t size, int flags)
+{
 	printf("wrapper rsetxattr\n");
 	return 0;
 }
 
-int wrapper_getxattr(const char *path, const char *name, char *value, size_t size) {
+int wrapper_getxattr(const char *path, const char *name, char *value, size_t size)
+{
 	printf("wrapper getxattr\n");
 	return 0;
 }
 
-int wrapper_listxattr(const char *path, char *list, size_t size) {
+int wrapper_listxattr(const char *path, char *list, size_t size)
+{
 	printf("wrapper listxattr\n");
 	return 0;
 }
 
-int wrapper_removexattr(const char *path, const char *name) {
+int wrapper_removexattr(const char *path, const char *name)
+{
 	printf("wrapper removexattr\n");
 	return 0;
 }
 
-int wrapper_opendir(const char *path, struct fuse_file_info *fileInfo) {
+int wrapper_opendir(const char *path, struct fuse_file_info *fileInfo)
+{
 	printf("wrapper opendir\n");
 	return 0;
 }
 
-int wrapper_readdir(const char *path, void *buf, fuse_fill_dir_t filler, off_t offset, struct fuse_file_info *fileInfo, fuse_readdir_flags rflags) {
-	printf("wrapper readdir\n");
-	return 0;
+int wrapper_readdir(const char *path, void *buf, fuse_fill_dir_t filler, off_t offset, struct fuse_file_info *fileInfo, fuse_readdir_flags rflags)
+{
+	int responseCode;
+	std::string pathValue(path);
+	std::list<DirEntry> dirEntries = nfsClient.read_directory(pathValue, responseCode);
+	for (auto const &dirEntry : dirEntries)
+	{
+		filler(buf, dirEntry.name.c_str(), &dirEntry.st, 0, FUSE_FILL_DIR_PLUS);
+	}
+	return responseCode;
 }
 
-int wrapper_releasedir(const char *path, struct fuse_file_info *fileInfo) {
+int wrapper_releasedir(const char *path, struct fuse_file_info *fileInfo)
+{
 	printf("wrapper releasedir\n");
 	return 0;
 }
 
-int wrapper_fsyncdir(const char *path, int datasync, struct fuse_file_info *fileInfo) {
+int wrapper_fsyncdir(const char *path, int datasync, struct fuse_file_info *fileInfo)
+{
 	printf("wrapper fsyncdir\n");
 	return 0;
 }
 
-int wrapper_init(struct fuse_conn_info *conn) {
+int wrapper_init(struct fuse_conn_info *conn)
+{
 	printf("wrapper init\n");
 	return 0;
 }
-
